c1216b7116efe30541e9056b6ec6f8d6b69ef804
Ksenia Yarysh <ksana32@gmail.com>
2019-02-18 10:26:16 +0500

    main class

---
+++ MainClass.java
@@ -0,0 +1,38 @@
+package lesson_4;
+
+public class MainClass {
+    public static void main(String[] args) {
+        GameClass g = new GameClass(5, 4);
+        g.printMap();
+        while (true) {
+
+            g.humanTurn();
+            g.printMap();
+            if (!g.checkWin(g.getHumanDot())) {
+                System.out.println("checking...no");
+            } else {
+                System.out.println("You win!");
+                break;
+            }
+            if (g.isMapFull()){
+                System.out.println("dead heat");
+                break;
+            }
+            g.blabla();
+
+            g.aiTurn();
+            g.printMap();
+            if (!g.checkWin(g.getAiDot())) {
+                System.out.println("checking...no");
+            } else {
+                System.out.println("I win!");
+                break;
+            }
+            if (g.isMapFull()){
+                System.out.println("dead heat");
+                break;
+            }
+
+        }
+    }
+}
